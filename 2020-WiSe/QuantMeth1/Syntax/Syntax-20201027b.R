06b7e20385f394c7c4bc388cb4b1785efff91184
Sebastian Sauer <ssauer@posteo.de>
2020-10-27 17:11:03 +0100

    add syntax

---
+++ 2020-WiSe/QuantMeth1/Syntax/Syntax-20201027b.R
@@ -0,0 +1,76 @@
+1+1  # Strg-Enter führt die Zeile aus
+
+2+2
+3+3
+4+4
+5+5
+
+
+42
+
+loesung <- 42 # Strg-Enter
+
+temperatur <- 9
+
+
+loesung
+
+
+x <- 99
+y <- 1
+
+x = y
+x <- y
+x -> y
+
+
+
+x <- 10
+
+studi <- "yeah"
+
+beruf <- "studi"  # Strg-Enter
+
+beruf <- studi
+
+
+mean(9)
+mean(temperatur)  # Mittelwert
+
+
+temp_mean <- mean(temperatur)
+
+help(mean)
+
+mean(x = temperatur)
+
+mean(x = temperatur, trim = 0, na.rm = FALSE)  # verständlicher
+mean(temperatur, 0, FALSE)
+
+mean(temperatur, FALSE, 0)
+
+
+x > y
+
+z > q
+
+x > 99
+
+
+x != y  # ungleich
+
+x == y  # Prüfung auf Gleichheit
+
+
+pi <- 3.14149265
+
+
+temperaturen <- c( 32 ,   54, 0)
+
+mean(temperaturen)
+
+(temperaturen - 32) * 5/9
+
+mean(  # Befehle immer abschließen, sonst gibt's Fehler.
+
+  
