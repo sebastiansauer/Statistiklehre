97fa9aafed19ea5a89232539600aeb6cf26d73a4
Sebastian Sauer <ssauer@posteo.de>
2020-10-30 17:43:25 +0100

    add datasets

--- 2020-WiSe/QuantMeth1/Syntax/Syntax-20201029b.R
+++ 2020-WiSe/QuantMeth1/Syntax/Syntax-20201029b.R
@@ -20,3 +20,38 @@ klausur[4,2] # Zeile, Spalte
 
 str(studis)
 str(klausur)
+
+
+studis[c(1,2)]
+
+studis[c(1,2,1,1,1,1,1)]
+
+
+bestanden3 <- c("Studi1" =  "ja",
+                "Studi2" = "nein",
+                "Studi3" =  "ja",
+                "Studi4" = "ja")
+
+bestanden3
+
+bestanden3[1]
+bestanden3["Studi1"]
+
+bestanden3[c(TRUE, FALSE, TRUE, FALSE)]
+
+bestanden3 == "ja"
+
+
+bestanden3[c(TRUE, FALSE, TRUE, TRUE)]
+bestanden3[bestanden3 == "ja"]
+
+
+
+test <- factor(c("#", "ä", "a", "A", "1", "!"))
+str(test)
+levels(test)
+
+
+
+x <- C("Anna", "Berta" "Carla")
+x <- c("Anna", "Berta", "Carla")
