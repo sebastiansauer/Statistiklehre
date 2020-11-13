dde367595520323b92c7bb17d7710986945720ef
sebastian.sauer <ssauer@posteo.de>
2020-11-13 18:45:07 +0000

    train-testdaten-einlesen.r

---
+++ 2020-WiSe/WissMeth/Pruefung/data/Train-Testdaten-einlesen.R
@@ -0,0 +1,5 @@
+traindata <- read.csv2("https://raw.githubusercontent.com/sebastiansauer/Statistiklehre/main/2020-WiSe/WissMeth/Pruefung/data/Anwendungsdaten.csv")
+
+
+
+testdata <- read.csv2("https://raw.githubusercontent.com/sebastiansauer/Statistiklehre/main/2020-WiSe/WissMeth/Pruefung/data/Trainingsdaten.csv")
